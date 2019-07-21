bfbb75a341ae247ba68d1753a9e127f127f671c2
markwilde33 <markwilde29@gmail.com>
2019-07-21 16:26:55 +0100

    Completed logic for displaying breadcrumb menu and child pge headings across pages

--- page.php
+++ page.php
@@ -14,19 +14,38 @@
 
   <div class="container container--narrow page-section">
     <?php
-    
-    ?>
-    <div class="metabox metabox--position-up metabox--with-home-link">
-      <p><a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title() ?></span></p>
+    $theParent = wp_get_post_parent_id(get_the_ID());
+    if ($theParent) { ?>
+      <div class="metabox metabox--position-up metabox--with-home-link">
+      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title() ?></span></p>
     </div>
     
-    <!-- <div class="page-links">
-      <h2 class="page-links__title"><a href="#">About Us</a></h2>
+
+    <?php }
+    ?>
+    <?php 
+    $testArray = get_pages(array('child_of' => get_the_ID()));
+
+    if ($theParent or $testArray) { ?>
+    <div class="page-links">
+      <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent) ?>"><?php echo get_the_title($theParent); ?></a></h2>
       <ul class="min-list">
-        <li class="current_page_item"><a href="#">Our History</a></li>
-        <li><a href="#">Our Goals</a></li>
+      <?php 
+      if ($theParent) {
+         $getChildren = $theParent;
+      } else {
+         $getChildren = get_the_ID();
+      }
+
+       wp_list_pages(array(
+         'title_li' => NULL,
+         'child_of' => $getChildren,
+         'sort_column' => 'menu_order'
+       ));
+       ?>
       </ul>
-    </div> -->
+    </div>
+      <?php } ?>
 
     <div class="generic-content">
       <?php the_content() ?>
