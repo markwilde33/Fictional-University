785599d87fbc48fefde8624d3ced8032bc54fec3
markwilde33 <markwilde29@gmail.com>
2019-07-27 15:46:39 +0100

    fixed some link and styling issues across blog pages, parents and children.

--- footer.php
+++ footer.php
@@ -16,9 +16,9 @@
             <nav class="nav-list">
               <ul>
                 <li><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
-                <li><a href="#">Programs</a></li>
-                <li><a href="#">Events</a></li>
-                <li><a href="#">Campuses</a></li>
+                <li><a href="<?php echo site_url('/'); ?>">Programs</a></li>
+                <li><a href="<?php echo site_url('/'); ?>">Events</a></li>
+                <li><a href="<?php echo site_url('/'); ?>">Campuses</a></li>
               </ul>
             </nav>
           </div>
@@ -27,9 +27,9 @@
             <h3 class="headline headline--small">Learn</h3>
             <nav class="nav-list">
               <ul>
-                <li><a href="#">Legal</a></li>
+                <li><a href="<?php echo site_url('/'); ?>">Legal</a></li>
                 <li><a href="<?php echo site_url('/privacy-policy') ?>">Privacy</a></li>
-                <li><a href="#">Careers</a></li>
+                <li><a href="<?php echo site_url('/'); ?>">Careers</a></li>
               </ul>
             </nav>
           </div>
--- header.php
+++ header.php
@@ -15,10 +15,10 @@
         <nav class="main-navigation">
          <ul>
             <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 15)echo 'class= "current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
-            <li><a href="#">Programs</a></li>
-            <li><a href="#">Events</a></li>
-            <li><a href="#">Campuses</a></li>
-            <li><a href="#">Blog</a></li>
+            <li><a href="<?php echo site_url('/'); ?>">Programs</a></li>
+            <li><a href="<?php echo site_url('/'); ?>">Events</a></li>
+            <li><a href="<?php echo site_url('/'); ?>">Campuses</a></li>
+            <li <?php if (get_post_type()  == 'post') echo 'class= "current-menu-item"' ?>><a href="<?php echo site_url('/'); ?>">Blog</a></li>
           </ul>
         </nav>
         <div class="site-header__util">
